7381a3c3f33c917c66bdb4b61f368e49b900c362
Mikhail Khodak <werewolfdevlearn@gmail.com>
2023-06-06 11:15:43 +0300

    modify notification

--- src/utils/notify-MD.ts
+++ src/utils/notify-MD.ts
@@ -1,15 +1,26 @@
-import { isRejected, isFulfilled } from '@reduxjs/toolkit';
+import { isRejected } from '@reduxjs/toolkit';
 
 import { toast } from 'react-toastify';
 import { Middleware } from 'redux';
+const regExp = {
+  userRegFF: 'user/Register/fulfilled',
+  userLoginFF: 'user/Login/fulfilled',
+  userLogOutFF: 'user/LogOut/fulfilled',
+};
 
-export const ErrorLogger: Middleware = (api) => (next) => (action) => {
+export const ErrorLogger: Middleware = (_api) => (next) => (action) => {
   console.log(action);
+
   if (isRejected(action)) {
-    toast.error(action.payload);
+    toast.error(`${action.payload}.
+   Please change your data and repeat you request`);
   }
-  if (isFulfilled(action)) {
-    toast.success(action.payload);
+
+  if (action.type === regExp.userRegFF || action.type === regExp.userLoginFF)
+    toast.success('Welcome!');
+
+  if (action.type === regExp.userLogOutFF) {
+    toast.success('Have a nice day!');
   }
 
   return next(action);
